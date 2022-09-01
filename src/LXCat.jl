9f89d8731b2f808e27a885ebfd70906f1caa5909
Christian Busch <christian.busch@ruhr-uni-bochum.de>
2022-09-01 20:07:42 +0200

    Implemented ions aswell and made the code more readable and shorter. Interfaces need to be adjusted

--- src/LXCat.jl
+++ src/LXCat.jl
@@ -1,12 +1,16 @@
 module LXCat
 
-using Dates
 using Interpolations
+using FileIO
+using DelimitedFiles
+using CairoMakie
 
-export load_database, parse_string
+export load_LXCat_file
 export ElasticCrossSection, EffectiveCrossSection, ExcitationCrossSection,
-       IonizationCrossSection
+       IonizationCrossSection, BackscatCrossSection, IsotropicCrossSection
 
+
+const keywords = ["ELASTIC", "EXCITATION", "IONIZATION", "EFFECTIVE"]
 
 abstract type AbstractCrossSection end;
 
@@ -17,16 +21,12 @@ end
 struct ElasticCrossSection{T} <: AbstractCrossSection 
     ground_state::String
     mass_ratio::Float64
-    comment::String
-    updated::DateTime
     cross_section::T
 end
 
 struct EffectiveCrossSection{T} <: AbstractCrossSection 
     ground_state::String
     mass_ratio::Float64
-    comment::String
-    updated::DateTime
     cross_section::T
 end
 
@@ -34,8 +34,6 @@ struct ExcitationCrossSection{T} <: AbstractCrossSection
     ground_state::String
     excited_state::String
     threshold_energy::Float64
-    comment::String
-    updated::DateTime
     cross_section::T
 end
 
@@ -43,143 +41,121 @@ struct IonizationCrossSection{T} <: AbstractCrossSection
     ground_state::String
     excited_state::String
     threshold_energy::Float64
-    comment::String
-    updated::DateTime
+    cross_section::T
+end
+
+struct BackscatCrossSection{T} <: AbstractCrossSection
+    cross_section::T
+end
+
+struct IsotropicCrossSection{T} <: AbstractCrossSection
     cross_section::T
 end
 
 
-function parse_string(s)
-    lines = split(s, '\n')
+function load_LXCat_file(file_path, particle_type::Symbol)
+    # options for particle_type are :electron, :ion
 
+    # read in LXCAT file 
+    lines = readlines(file_path)
 
-    # find start and end lines cross section data
-    cs_start = findfirst(x -> startswith(x, "--"), lines)
-    cs_end = findlast(x -> startswith(x, "--"), lines)
-
-    comment = ""
-    updated_str = ""
-    for l in lines[3:cs_start]
-        if startswith(l, "COMMENT")
-            comment *= replace(l, "COMMENT: " => "")
-        end
-        if startswith(l, "UPDATED")
-            updated_str = replace(l, "UPDATED: " => "", )
-            updated_str = replace(updated_str, " " => "T")
-        end
-    end
+    # extract the relevant lines.
+    header_lines, cs_data_lines = split_lines_into_header_and_cs_data(lines, particle_type)
     
-    energy = Float64[]
-    cs = Float64[]
-    for l in lines[(cs_start + 1):(cs_end - 1)]
-        (e, c) = split(strip(l), '\t')
-        push!(energy, parse(Float64, e))
-        push!(cs, parse(Float64, c))
+    cross_sections = []
+    for (header, cs_data) = zip(header_lines, cs_data_lines)
+        # parse the energies and corresponding cross sections into vectors
+        energies, σ = parse_cross_sections_from_lines(cs_data)
+
+        # create the corresponding cross section struct based on information in the header lines
+        cs_struct   = create_cs_struct(header, energies, σ, particle_type)
+        
+        push!(cross_sections, cs_struct)
     end
-    # sorting by energy, if the data is not in the right order
-    perm = sortperm(energy)
-    energy = Interpolations.deduplicate_knots!(energy[perm])
-    cs = cs[perm]
 
-    if lines[1] == "IONIZATION"
-        # ground_state is given by the line after EFFECTIVE
-        (ground_state, excited_state) = split(lines[2], "->")
-        # threshold_energy is given by the next line
-        threshold_energy = parse(Float64, strip(split(lines[3], '/')[1]))
-        IonizationCrossSection(
-            String(strip(ground_state)), 
-            String(strip(excited_state)),
-            threshold_energy, 
-            comment, 
-            DateTime(updated_str), 
-            LinearInterpolation(energy, cs, extrapolation_bc=Line())
-        )
-    elseif lines[1] == "EXCITATION"
-        # ground_state is given by the line after EFFECTIVE
-        (ground_state, excited_state) = split(lines[2], "->")
-        # threshold_energy is given by the next line
-        threshold_energy = parse(Float64, strip(split(lines[3], '/')[1]))
-        ExcitationCrossSection(
-            String(strip(ground_state)), 
-            String(strip(excited_state)),
-            threshold_energy, 
-            comment, 
-            DateTime(updated_str), 
-            LinearInterpolation(energy, cs, extrapolation_bc=Line())
-        )
-    elseif lines[1] == "ELASTIC"
-        # ground_state is given by the line after EFFECTIVE
-        ground_state = String(lines[2])
-
-        # mass_ratio is given by the next line
-        mass_ratio = parse(Float64, strip(split(lines[3], '/')[1]))
-
-        ElasticCrossSection(
-            ground_state, 
-            mass_ratio, 
-            comment, 
-            DateTime(updated_str), 
-            LinearInterpolation(energy, cs, extrapolation_bc=Line())
-        )
-    elseif lines[1] == "EFFECTIVE"
-        # ground_state is given by the line after EFFECTIVE
-        ground_state = String(lines[2])
-
-        # mass_ratio is given by the next line
-        mass_ratio = parse(Float64, strip(split(lines[3], '/')[1]))
-
-        EffectiveCrossSection(
-            ground_state, 
-            mass_ratio, 
-            comment, 
-            DateTime(updated_str), 
-            LinearInterpolation(energy, cs, extrapolation_bc=Line())
-        )
-    end
+    return cross_sections
 end
 
+function split_lines_into_header_and_cs_data(lines, particle_type:: Symbol)
+    # find the start of the cs data by looking for keywords
+    if particle_type == :ion
+        header_starts   = findall(l -> occursin("SPECIES", l), lines)
+    elseif particle_type == :electron
+        header_starts   = findall(l -> any(l .== keywords), lines)
+    end
 
-const KEYWORD_DICT = Dict(
-                        "ELASTIC" => ElasticCrossSection,
-                        "EFFECTIVE" => EffectiveCrossSection,
-                        "EXCITATION" => ExcitationCrossSection,
-                        "IONIZATION" => IonizationCrossSection
-)
+    # cs data starts and ends with a number of ----------        
+    dash_dash_pos = findall(l -> occursin("---------------", l), lines) 
+    cs_starts     = dash_dash_pos[1:2:end]  .+ 1                                  
+    cs_ends       = dash_dash_pos[2:2:end]  .- 1
 
+    header_lines  = []
+    cs_data_lines = []
 
-function load_database(filename; ground_state=nothing)
-    cross_sections = Dict()
-    cs_string = ""
-    sep_counter = -1
-	open(filename) do file 
-		for line in eachline(file)
-            if strip(line) in keys(KEYWORD_DICT)
-                cs_string = ""
-                sep_counter = 0
-			end
-            if sep_counter >= 0
-                cs_string *= line * '\n'
-                if startswith(line, "---")
-                    sep_counter += 1
-                end
-                if sep_counter == 2
-                    cs = parse_string(cs_string)
-                    if ground_state === nothing || cs.ground_state == ground_state
-                        if cs isa ElasticCrossSection
-                            cross_sections[(cs.ground_state, "elastic")] = cs
-                        elseif cs isa EffectiveCrossSection
-                            cross_sections[(cs.ground_state, "effective")] = cs
-                        elseif cs isa ExcitationCrossSection
-                            cross_sections[(cs.ground_state, cs.excited_state)] = cs
-                        elseif cs isa IonizationCrossSection
-                            cross_sections[(cs.ground_state, cs.excited_state)] = cs
-                        end
-                    end
-                end
-            end
-		end
-	end
-    cross_sections
+    for (header_start, cs_data_start, cs_data_end) = zip(header_starts, cs_starts, cs_ends)
+        push!(header_lines,  lines[header_start:cs_data_start])
+        push!(cs_data_lines, lines[cs_data_start:cs_data_end])
+    end
+
+    return header_lines, cs_data_lines
+end
+
+function parse_cross_sections_from_lines(cs_data_lines)
+    N         = length(cs_data_lines)
+    energies  = Float64[]
+    σ         = Float64[]
+
+    for (e_substring, cs_substring) in split.(cs_data_lines,"\t")
+
+        e  = parse(Float64, String(e_substring))
+        cs = parse(Float64, String(cs_substring))
+
+        push!(energies, e)
+        push!(σ, cs)
+    end
+
+    return energies, σ
+end
+
+function create_cs_struct(header_lines, energies, σ, particle_type::Symbol)
+    
+    if particle_type == :electron
+        crossection_type = header_lines[1]
+    elseif particle_type == :ion
+        crossection_type = strip(String(split(header_lines[2], ",")[2]))
+    end
+
+    if crossection_type == "EXCITATION"
+        (ground_state, excited_state) = split(header_lines[2], "->")
+        ground_state                  = String(strip(ground_state))
+        excited_state                 = String(strip(excited_state))
+        threshold_energy              = parse(Float64, header_lines[3])
+        cs_interpolation              = LinearInterpolation(energies, σ, extrapolation_bc=Line())
+        return ExcitationCrossSection(ground_state, excited_state, threshold_energy, cs_interpolation)
+    elseif crossection_type == "ELASTIC"
+        ground_state                  = String(header_lines[2])
+        mass_ratio                    = parse(Float64, header_lines[3])
+        cs_interpolation              = LinearInterpolation(energies, σ, extrapolation_bc=Line())
+        return ElasticCrossSection(ground_state, mass_ratio, cs_interpolation)
+    elseif crossection_type == "IONIZATION"
+        (ground_state, excited_state) = split(header_lines[2], "->")
+        ground_state                  = String(strip(ground_state))
+        excited_state                 = String(strip(excited_state))
+        threshold_energy              = parse(Float64, header_lines[3])
+        cs_interpolation              = LinearInterpolation(energies, σ, extrapolation_bc=Line())
+        return IonizationCrossSection(ground_state, excited_state, threshold_energy, cs_interpolation)
+    elseif crossection_type == "EFFECTIVE"
+        ground_state                  = String(header_lines[2])
+        mass_ratio                    = parse(Float64, header_lines[3])
+        cs_interpolation              = LinearInterpolation(energies, σ, extrapolation_bc=Line())
+        return EffectiveCrossSection(ground_state, mass_ratio, cs_interpolation)
+    elseif crossection_type == "Backscat"
+        cs_interpolation              = LinearInterpolation(energies, σ, extrapolation_bc=Line())
+        return BackscatCrossSection(cs_interpolation)
+    elseif crossection_type == "Isotropic"
+        cs_interpolation              = LinearInterpolation(energies, σ, extrapolation_bc=Line())
+        return IsotropicCrossSection(cs_interpolation)
+    end
 end
 
 end # module

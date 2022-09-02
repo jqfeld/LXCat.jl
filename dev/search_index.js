3a67201d84fea15187e8649e4688e28d05ec0f97
Documenter.jl <documenter@juliadocs.github.io>
2022-09-02 14:18:24 +0000

    build based on 7d105c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LXCat","category":"page"},{"location":"#LXCat","page":"Home","title":"LXCat","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LXCat.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LXCat]","category":"page"}]
+}

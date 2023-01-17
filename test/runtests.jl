dd7b5e4c81306e9ca64d5e73b20a6e630c26747b
Florens Grimm <grimm@aept.ruhr-uni-bochum.de>
2023-01-17 17:12:12 +0100

    Adds tests for Attachment collision

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,6 @@ using LXCat, Test, SafeTestsets
 @safetestset "String Parsing Ionization Cross Section" begin include("string_parse_ionization_test.jl") end
 @safetestset "String Parsing Isotropic Cross Section" begin include("string_parse_isotropic_test.jl") end
 @safetestset "String Parsing BackScatter Cross Section" begin include("string_parse_backscatter_test.jl") end
+@safetestset "String Parsing Attachment Cross Section" begin include("string_parse_attachment_test.jl") end
 
 @safetestset "Database Parsing" begin include("load_database_test.jl") end
